649b547bea8a2259357ea9d0d22d014a1750c770
デザミシステム <development@dezamisystem.com>
2020-07-01 10:20:32 +0900

    Update screen shot

--- Assets/Editor/CustomPostProcessBuild.cs
+++ Assets/Editor/CustomPostProcessBuild.cs
@@ -1,4 +1,8 @@
-﻿using System.IO;
+﻿/*
+ * CustomPostProcessBuild.cs
+ * Copyright (c) 2020 東亜プリン秘密研究所. All rights reserved.
+ */
+using System.IO;
 using UnityEditor;
 using UnityEditor.Callbacks;
 using UnityEditor.iOS.Xcode;
